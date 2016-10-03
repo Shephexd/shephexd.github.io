90c8c3692d1db4a006ad43d63cf3fe23693a5995
shephexd <shephexd@gmail.com>
2016-10-03 08:43:20 +0300

    design setting changed

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,21 +9,20 @@
     $(document).ready(function(){
 
         $(".post-content").fitVids();
-        
+
         // Calculates Reading Time
         $('.post-content').readingTime({
             readingTimeTarget: '.post-reading-time',
             wordCountTarget: '.post-word-count',
         });
-        
+
         // Creates Captions from Alt tags
         $(".post-content img").each(function() {
             // Let's put a caption if there is one
-            if($(this).attr("alt") && !$(this).hasClass("emoji"))
-              $(this).wrap('<figure class="image"></figure>')
-              .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
+            if($(this).attr("alt") && !$(this).parent().hasClass("image"))
+              $(this).wrap('<figure class="image"></figure>').after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
         });
-        
+
     });
 
 }(jQuery));
